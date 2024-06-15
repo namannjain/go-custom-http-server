db9b82e6bfada856572a1777e1ad709d5dbdf87f
Naman Jain <naman.jain@msbdocs.com>
2024-06-15 19:44:49 +0530

    pass stage

--- app/server.go
+++ app/server.go
@@ -4,7 +4,18 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
+
+func handleConnection(conn net.Conn) {
+	buffer := make([]byte, 1024)
+	conn.Read(buffer)
+	if strings.HasPrefix(string(buffer), "GET / HTTP/1.1") {
+		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	} else {
+		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+	}
+}
 
 func main() {
 	ln, err := net.Listen("tcp", "0.0.0.0:4221")
@@ -18,8 +29,6 @@ func main() {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
-	_, err = conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-	if err != nil {
-		fmt.Println("Error writing to connection: ", err.Error())
-	}
+
+	handleConnection(conn)
 }
