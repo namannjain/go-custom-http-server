60d25e843eed3ee9ee2026817c19b77f4bfd0f61
Naman Jain <naman.jain@msbdocs.com>
2024-06-15 18:33:20 +0530

    pass stage

--- app/server.go
+++ app/server.go
@@ -7,15 +7,19 @@ import (
 )
 
 func main() {
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
+	ln, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
+	conn, err := ln.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
+	}
+	_, err = conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	if err != nil {
+		fmt.Println("Error writing to connection: ", err.Error())
 	}
 }
